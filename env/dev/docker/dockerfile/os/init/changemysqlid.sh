77ea4285fcfb6d7a05d69776f459eec4592904b8
neoericnet <neoericnet@163.com>
2019-12-25 10:03:46 +0800

    update change id shell

--- env/dev/docker/dockerfile/os/init/changemysqlid.sh
+++ env/dev/docker/dockerfile/os/init/changemysqlid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set +e
 
 ##define vars
 new_mysql_uid=$1
@@ -10,5 +10,5 @@ old_mysql_gid=$(id -g mysql)
 ##change
 usermod -u $new_mysql_uid mysql
 groupmod -g $new_mysql_gid mysql
-find / -user $old_mysql_uid -exec chown -h mysql {} \;
-find / -group $old_mysql_gid -exec chgrp -h mysql {} \;
+chown -R mysql:mysql /soft/mysql
+chown -R mysql:mysql /data/mysql
